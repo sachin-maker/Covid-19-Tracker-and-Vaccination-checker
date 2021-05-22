04c3cf8715a95751cbc2dd3d825ec4d05e5adbe9
Sachin <sachindeshpande456@gmail.com>
2021-05-22 09:49:16 +0530

    Covid-19 Tracker and Vaccination Checker

---
+++ app/src/main/java/com/sachin/covid_19trackerandvaccinechecker/AboutActivity.java
@@ -0,0 +1,44 @@
+package com.sachin.covid_19trackerandvaccinechecker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.NumberFormat;
+
+public class AboutActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+
+
+    }
+
+
+}
